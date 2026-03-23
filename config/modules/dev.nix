63acfb7b7c72434499aaa4a2a20932af5e636aa0
karma <captainav0608@gmail.com>
2026-03-23 11:56:49 +0530

    Revert "[feat] update dev.nix to add docker daemon support"
    This reverts commit d6e1b3d6e35f64dadf5592dd07f955dd73b55865.

--- config/modules/dev.nix
+++ config/modules/dev.nix
@@ -14,27 +14,4 @@
     "$HOME/.npm-global/bin"
     "$HOME/go/bin"
   ];
-
-  home.sessionVariables = {
-    DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/docker.sock";
-  };
-
-  systemd.user.services.docker = {
-    Unit = {
-      Description = "Docker Application Container Engine (Rootless)";
-    };
-    Service = {
-      Environment = [ "PATH=${pkgs.docker}/bin:/bin:/usr/bin:/sbin:/usr/sbin" ];
-      ExecStart = "${pkgs.docker}/bin/dockerd-rootless.sh --experimental";
-      ExecReload = "/bin/kill -s HUP $MAINPID";
-      LimitNOFILE = 1048576;
-      LimitNPROC = "infinity";
-      LimitCORE = "infinity";
-      Delegate = "yes";
-      KillMode = "mixed";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
 }
